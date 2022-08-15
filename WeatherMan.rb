6e1f52391f8bc935ec191170b440344f24dea093
danish <muhammad.danish@devsinc.com>
2022-08-15 10:40:36 -0400

    resolved formatting issues

--- WeatherMan.rb
+++ WeatherMan.rb
@@ -1,62 +1,53 @@
+# frozen_string_literal: true
 
 require 'colorize'
 require 'date'
-require './modules/weather_module.rb'
+require './modules/weather_module'
 
-
-#if the first Command line argument is "-a" this will execute
-if ARGV[0] == "-a"
-  year,month=ARGV[1].split('/')
+case ARGV[0]
+when '-a'
+  year, month = ARGV[1].split('/')
   dir = ARGV[2]
-  Weather_module.month_temperature(dir,year,month)
-end
+  Weather_module.month_temperature(dir, year, month)
+when '-e'
+  year = ARGV[1]
+  path = ARGV[2]
 
-# if the first command line argument is "-e" this will execute
-if ARGV[0] == "-e"
-  year=ARGV[1]
-  path=ARGV[2]
+  max, min, max_humid = Weather_module.year_temperature(path, year)
 
-  max,min,max_humid=Weather_module.year_temperature(path,year)
-
-  max.each do |key,value|
+  max.each do |key, value|
     puts "Highest:#{value}C on #{DateTime.parse(key.to_s).strftime('%B %d')}"
   end
-  min.each do |key,value|
+  min.each do |key, value|
     puts "Lowest:#{value}C on #{DateTime.parse(key.to_s).strftime('%B %d')}"
   end
-  max_humid.each do |key,value|
+  max_humid.each do |key, value|
     puts "Humid:#{value}% on #{DateTime.parse(key.to_s).strftime('%B %d')}"
   end
-
-end
-
-# if the first command line argument is "-e" this will execute
-if ARGV[0] == "-c"
-  year,month=ARGV[1].split('/')
+when '-c'
+  year, month = ARGV[1].split('/')
   dir = ARGV[2]
-  min_temp,max_temp= Weather_module.month_temperature_bar(dir,year,month)
-    min_temp.each_with_index do |min,index|
-      print "#{index+1} "
-      max_temp[index].times{print "+".colorize(:red)}
-      print "#{max_temp[index]}\n"
-      print "#{index+1} "
-      min_temp[index].times{print "+".colorize(:blue)}
-      print "#{min_temp[index]}\n"
-    end
-end
-# if the first command line argument is "-r" this will execute
-
-if ARGV[0] == "-r"
-  year,month=ARGV[1].split('/')
+  min_temp, max_temp = Weather_module.month_temperature_bar(dir, year, month)
+  min_temp.each_with_index do |_min, index|
+    print "#{index + 1} "
+    max_temp[index].times { print '+'.colorize(:red) }
+    print "#{max_temp[index]}\n"
+    print "#{index + 1} "
+    min_temp[index].times { print '+'.colorize(:blue) }
+    print "#{min_temp[index]}\n"
+  end
+when '-r'
+  year, month = ARGV[1].split('/')
   dir = ARGV[2]
-  min_temperature,max_temperature=Weather_module.month_temperature_bar(dir,year,month)
+  min_temperature, max_temperature = Weather_module.month_temperature_bar(dir, year, month)
 
-  min_temperature.each_with_index do |min,index|
-    print "#{index+1} "
-    min_temperature[index].times{print "+".colorize(:blue)}
-    max_temperature[index].times{print "+".colorize(:red)}
+  min_temperature.each_with_index do |_min, index|
+    print "#{index + 1} "
+    min_temperature[index].times { print '+'.colorize(:blue) }
+    max_temperature[index].times { print '+'.colorize(:red) }
     print "#{min_temperature[index]}C-#{max_temperature[index]}C"
     print "\n"
   end
+else
+  puts 'Wrong number of arguments'
 end
-
--- classes/weather.rb
+++ classes/weather.rb
@@ -1,27 +1,9 @@
+# frozen_string_literal: true
+
 # class to hold weather data  of single day
 class Weather
-  attr_accessor :max_temp_C
-  attr_accessor :mean_temp_C
-  attr_accessor :min_temp_C
-  attr_accessor :dew_point_C
-  attr_accessor :mean_dew_point_C
-  attr_accessor :min_dew_point_C
-  attr_accessor :mean_humidity
-  attr_accessor :min_humidity
-  attr_accessor :max_pressure_Pa
-  attr_accessor :mean_pressure_Pa
-  attr_accessor :min_pressure_Pa
-  attr_accessor :max_visibility
-  attr_accessor :mean_visibility
-  attr_accessor :min_visibility
-  attr_accessor :max_wind_speed
-  attr_accessor :mean_wind_speed
-  attr_accessor :max_gust_speed
-  attr_accessor :max_humidity
-  attr_accessor :precipitationmm
-  attr_accessor :cloud_cover
-  attr_accessor :events
-  attr_accessor :wind_dir_degrees
+  attr_accessor :max_temp_C, :mean_temp_C, :min_temp_C, :dew_point_C, :mean_dew_point_C, :min_dew_point_C,
+                :mean_humidity, :min_humidity, :max_pressure_Pa, :mean_pressure_Pa, :min_pressure_Pa, :max_visibility, :mean_visibility, :min_visibility, :max_wind_speed, :mean_wind_speed, :max_gust_speed, :max_humidity, :precipitationmm, :cloud_cover, :events, :wind_dir_degrees
 
   def initialize(
     max_temp_C,
@@ -31,7 +13,7 @@ class Weather
     mean_dew_point_C,
     min_dew_point_C,
     max_humidity,
-    mean_humidity,
+    _mean_humidity,
     min_humidity,
     max_pressure_Pa,
     mean_pressure_Pa,
@@ -45,29 +27,30 @@ class Weather
     precipitationmm,
     cloud_cover,
     events,
-    wind_dir_degrees)
+    wind_dir_degrees
+  )
 
-      @max_temp_C = max_temp_C
-      @mean_temp_C = mean_temp_C
-      @min_temp_C = min_temp_C
-      @dew_point_C = dew_point_C
-      @mean_dew_point_C = mean_dew_point_C
-      @min_dew_point_C = min_dew_point_C
-      @max_humidity = max_humidity
-      @mean_humidity = max_humidity
-      @min_humidity = min_humidity
-      @max_pressure_Pa = max_pressure_Pa
-      @mean_pressure_Pa = mean_pressure_Pa
-      @min_pressure_Pa = min_pressure_Pa
-      @max_visibility = max_visibility
-      @mean_visibility = mean_visibility
-      @min_visibility = min_visibility
-      @max_wind_speed = max_wind_speed
-      @mean_wind_speed = mean_wind_speed
-      @max_gust_speed = max_gust_speed
-      @precipitationmm = precipitationmm
-      @cloud_cover = cloud_cover
-      @events = events
-      @wind_dir_degrees = wind_dir_degrees
+    @max_temp_C = max_temp_C
+    @mean_temp_C = mean_temp_C
+    @min_temp_C = min_temp_C
+    @dew_point_C = dew_point_C
+    @mean_dew_point_C = mean_dew_point_C
+    @min_dew_point_C = min_dew_point_C
+    @max_humidity = max_humidity
+    @mean_humidity = max_humidity
+    @min_humidity = min_humidity
+    @max_pressure_Pa = max_pressure_Pa
+    @mean_pressure_Pa = mean_pressure_Pa
+    @min_pressure_Pa = min_pressure_Pa
+    @max_visibility = max_visibility
+    @mean_visibility = mean_visibility
+    @min_visibility = min_visibility
+    @max_wind_speed = max_wind_speed
+    @mean_wind_speed = mean_wind_speed
+    @max_gust_speed = max_gust_speed
+    @precipitationmm = precipitationmm
+    @cloud_cover = cloud_cover
+    @events = events
+    @wind_dir_degrees = wind_dir_degrees
   end
 end
